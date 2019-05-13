0059f4f431564c289683117ce596f31672768f21
Eskild <eskild.emedd33@gmail.com>
2019-05-13 09:30:19 +0200

    initial commit

---
+++ blinking_signals.py
@@ -0,0 +1,77 @@
+import RPi.GPIO as GPIO 
+import time
+
+BUTTON_out=13
+BUTTON_in = 19
+RED_LED = 16
+YELLOW_LED = 12
+
+def SetUp():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(BUTTON_in,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(BUTTON_out,GPIO.OUT)
+
+    GPIO.setup(YELLOW_LED, GPIO.OUT) 
+    GPIO.setup(RED_LED, GPIO.OUT) 
+    for i in range(5):
+        GPIO.output(RED_LED,GPIO.HIGH)
+        GPIO.output(YELLOW_LED,GPIO.HIGH)
+        time.sleep(0.1)
+        GPIO.output(YELLOW_LED,GPIO.LOW)
+        GPIO.output(RED_LED,GPIO.LOW)
+        time.sleep(0.1)
+    GPIO.output(BUTTON_out,GPIO.HIGH)
+
+def blink401():
+    for i in range(2):
+        GPIO.output(RED_LED, GPIO.HIGH)
+        GPIO.output(YELLOW_LED, GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(RED_LED, GPIO.LOW)
+        GPIO.output(YELLOW_LED, GPIO.LOW)
+        time.sleep(0.5)
+
+def blink404():
+    for i in range(4):
+        GPIO.output(RED_LED, GPIO.HIGH)
+        GPIO.output(YELLOW_LED, GPIO.HIGH)
+        time.sleep(0.1)
+        GPIO.output(RED_LED, GPIO.LOW)
+        GPIO.output(YELLOW_LED, GPIO.LOW)
+
+def NoInternet():
+    for i in range(2):
+        GPIO.output(RED_LED, GPIO.HIGH)
+        GPIO.output(YELLOW_LED, GPIO.LOW)
+        time.sleep(0.5)
+        GPIO.output(RED_LED, GPIO.LOW)
+        GPIO.output(YELLOW_LED, GPIO.HIGH)
+        time.sleep(0.5)
+    GPIO.output(YELLOW_LED, GPIO.LOW)
+    GPIO.output(RED_LED, GPIO.LOW)
+
+def blinkbreakLoop():
+    GPIO.output(RED_LED, GPIO.HIGH)
+    GPIO.output(YELLOW_LED, GPIO.LOW)
+    time.sleep(5)
+    GPIO.output(RED_LED, GPIO.LOW)
+
+def blinkServerError():
+    for i in range(2):
+        GPIO.output(RED_LED, GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(RED_LED, GPIO.LOW)
+        time.sleep(0.5)
+
+    for i in range(2):
+        GPIO.output(YELLOW_LED, GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(YELLOW_LED, GPIO.LOW)
+        time.sleep(0.5)
+
+def blinkFifteenMinutes():
+    for i in range(2):
+        GPIO.output(RED_LED, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(RED_LED, GPIO.LOW)
+        time.sleep(1)
---
+++ buttonSendPush_v4.py
@@ -0,0 +1,64 @@
+from datetime import datetime
+from datetime import timedelta
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+GPIO.setwarnings(False) # Ignore warning for now
+import time
+import requests
+import json
+import simplejson
+from params import topic, notification_key, website_url
+
+from blinking_signals import SetUp, blink401, blinkbreakloop, blink404 NoInternet, blinkServerError, blinkFifteenMinutes
+# defining of parameters and function
+###############################################
+
+BUTTON_out=13
+BUTTON_in = 19
+RED_LED = 16
+YELLOW_LED = 12
+
+url = website_url
+pressed_time = datetime.now() - timedelta(-30)
+
+payload = {
+    "topic":topic,
+    "notification_key":notification_key,
+}
+headers = {
+    "Content-Type": "application/json",
+}
+
+# Main part of the script
+#######################################
+
+SetUp()
+while True:
+    try:
+    	GPIO.output(BUTTON_out, GPIO.HIGH)
+        input_value = GPIO.input(BUTTON_in)
+        if input_value:
+            elapsed_time = datetime.now() - pressed_time
+            if elapsed_time.total_seconds() > 900:
+                pressed_time = datetime.now()
+                GPIO.output(YELLOW_LED,GPIO.HIGH)
+                r = requests.post(url, data=json.dumps(payload), headers=headers, timeout=5)
+		        status_code = r.status_code
+                GPIO.output(YELLOW_LED,GPIO.LOW)
+		        if status_code == 201:
+                    pass
+                elif status_code == 401:
+                    blink401()
+                elif status_code == 404:
+                    blink404()
+                elif int(status_code) >=500
+                    blinkServerError()
+            else:
+                GPIO.output(YELLOW_LED,GPIO.LOW)
+                blinkFifteenMinutes()
+    except Exception as e:
+        filename = "errors/" + str(datetime.now())
+        logf = open(filename, "w")
+        logf.write('An exceptional thing happed - %s' % e)
+        brinkbreakloop()
+	    GPIO.cleanup()
+	    break
