ecbcef2305bf87984876cd2e51f179dfb0fd6634
Eskild <eskild.emedd33@gmail.com>
2019-05-13 10:40:58 +0200

    print status code

--- buttonSendPush_v4.py
+++ buttonSendPush_v4.py
@@ -44,6 +44,7 @@ while True:
                 GPIO.output(YELLOW_LED,GPIO.HIGH)
                 r = requests.post(url, data=json.dumps(payload), headers=headers, timeout=5)
                 status_code = r.status_code
+                print(status_code)
                 GPIO.output(YELLOW_LED,GPIO.LOW)
                 if status_code == 201:
                     print("success")
