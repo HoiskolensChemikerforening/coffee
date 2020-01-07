e3fbfcae8da3c2fa7d7a83bd009d1e8d04bd479f
Geirhov <46625651+Geirhov@users.noreply.github.com>
2020-01-07 15:35:46 +0100

    Update buttonSendPush_v4.py

--- buttonSendPush_v4.py
+++ buttonSendPush_v4.py
@@ -12,8 +12,8 @@ try:
     # defining of parameters
     ###############################################
 
-    BUTTON_out=5
-    BUTTON_in = 20
+    BUTTON_out=13
+    BUTTON_in = 19
     RED_LED = 16
     YELLOW_LED = 12
 
